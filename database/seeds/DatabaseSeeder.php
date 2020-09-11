8a70a26ea9d83b9562de38c986accc91a7c05f3f
SaintMiner <dinden290@gmail.com>
2020-09-11 13:14:04 +0300

    [Back-End] SystemSeeder Enabled

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            // SystemSeeder::class,
+            SystemSeeder::class,
             UserSeeder::class
         ]);
     }
